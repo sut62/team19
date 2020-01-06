6c891712fc33540f841ea443ebe9daadf34ab576
KritsanarakP <guidemg16@gmail.com>
2020-01-06 11:35:15 +0700

    add Controller for Carname - close #22

---
+++ backend/src/main/java/com/cpe/backend/Carname/Controller/ProvinceController.java
@@ -0,0 +1,32 @@
+package com.cpe.backend.Carname.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import com.cpe.backend.Carname.Entity.Province;
+import com.cpe.backend.Carname.Repository.ProvinceRepository;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:8081")
+@RestController
+public class ProvinceController {
+
+    @Autowired
+    private final ProvinceRepository provinceRepository;
+
+    public ProvinceController(ProvinceRepository provinceRepository) {
+        this.provinceRepository = provinceRepository;
+    }
+
+    @GetMapping("/province")
+    public Collection<Province> Provinces() {
+        return provinceRepository.findAll().stream().collect(Collectors.toList());
+    }
+
+}
