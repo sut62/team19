ac47184485ff722f915d1ad0c4bafdf1439969fe
Nattapol Panta <namenp2707@hotmail.com>
2020-01-23 22:55:33 +0700

    Edit Controller and Repository for Customer - close #106

--- backend/src/main/java/com/cpe/backend/Customer/Repository/CustomerRepository.java
+++ backend/src/main/java/com/cpe/backend/Customer/Repository/CustomerRepository.java
@@ -8,12 +8,11 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.data.repository.query.Param;
 import java.util.Collection;
 
-@CrossOrigin(origins = "http://localhost:8081")
 @RepositoryRestResource
 public
 interface CustomerRepository extends JpaRepository<Customer, Long> {
 	Customer findById(long id);
-	@Query( value = "SELECT * FROM Customer c where c.name = :name",
+	@Query( value = "SELECT * FROM Customer x where x.name = :name",
             nativeQuery = true)
-    Collection<Customer> findByNameCustomer(@Param("name") String name);
+    Collection<Customer> findCustomerByName(@Param("name") String name);
 }
